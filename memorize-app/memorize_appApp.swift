64386e3b266bd42ac15d75bc17d0f0719be36869
Lucas Polizeli <lumpolizeli@gmail.com>
2021-04-12 20:05:54 -0300

    Initial Commit

---
+++ memorize-app/memorize_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorize_appApp.swift
+//  memorize-app
+//
+//  Created by Lucas Polizeli on 12/04/21.
+//
+
+import SwiftUI
+
+@main
+struct memorize_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
