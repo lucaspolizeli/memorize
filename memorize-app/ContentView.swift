5c8871b5b8eec96ed431b0d3659004c7bceb8e76
Lucas Polizeli <lumpolizeli@gmail.com>
2021-04-12 21:18:20 -0300

    chore: first commit with project structure

--- memorize-app/ContentView.swift
+++ memorize-app/ContentView.swift
@@ -9,8 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .font(Font.largeTitle)
+        .foregroundColor(Color.orange)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool;
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+
+
+        }
     }
 }
 
